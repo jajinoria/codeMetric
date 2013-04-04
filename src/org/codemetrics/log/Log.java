6db4718480f98feeb005163a17c16571603d05dc
usuario <usuario@10.22.146.194>
2013-04-04 10:14:53 +0100

    Logger & Writer [04/04/13]

--- src/org/codemetrics/log/Log.java
+++ src/org/codemetrics/log/Log.java
@@ -6,7 +6,6 @@ package org.codemetrics.log;
  * @author Daniel & Jose & Johanna
  */
 public abstract class Log {
-    protected String fileName;
     
     public abstract void log();
 }
---
+++ src/org/codemetrics/log/MethodMetricLogger.java
@@ -0,0 +1,48 @@
+
+package org.codemetrics.log;
+
+import java.util.Date;
+import org.codemetrics.log.writer.Writer;
+import org.codemetrics.metricparser.MethodMetricParser;
+
+/**
+ *
+ * @author Daniel & Jose & Johanna
+ */
+public class MethodMetricLogger extends Log{
+    private MethodMetricParser methodMetricParser;
+    private Writer writer;
+    
+    public MethodMetricLogger (Writer writer){
+        this.methodMetricParser = new MethodMetricParser();
+        this.writer = writer;
+    }
+
+    public MethodMetricParser getMethodMetricParser() {
+        return methodMetricParser;
+    }
+
+    public void setMethodMetricParser(MethodMetricParser methodMetricParser) {
+        this.methodMetricParser = methodMetricParser;
+    }
+
+    public Writer getWriter() {
+        return writer;
+    }
+
+    public void setWriter(Writer writer) {
+        this.writer = writer;
+    }    
+
+    @Override
+    public void log() {
+        this.writer.writeTitle("Method metric"); 
+        this.writer.writeDate(new Date());
+        this.writer.writePackageName("org.gs2.test");
+        this.writer.writeClassName("Test");
+        this.writer.writeMethodName("BlahBlahBlah");
+        this.writer.writeNumberOfParameters(1);   // aquí irá methodMetricParser.getNumberOfParamters();
+        this.writer.close();                      // lo mismo con codelines;
+    }
+    
+}
