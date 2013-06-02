f8ba2c9f992926478036730300e29204e7dde77b
Jose <Jose@Deadwind>
2013-06-02 19:10:56 +0100

    working in CubeMetrics

--- src/org/codemetrics/saver/DataWereHouseSaver.java
+++ src/org/codemetrics/saver/DataWereHouseSaver.java
@@ -2,12 +2,15 @@
 package org.codemetrics.saver;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.codemetrics.cubes.definition.CubeDefinition;
 import org.codemetrics.cubes.definition.DataStoreDefinition;
 import org.codemetrics.cubes.definition.DimensionDefinition;
+import org.codemetrics.metricparser.ClassMetricParser;
+import org.codemetrics.metricparser.MethodMetricParser;
 import org.codemetrics.metricparser.PackageMetricParser;
 import org.sumus.dwh.cube.Cube;
 import org.sumus.dwh.datastore.Context;
@@ -88,13 +91,15 @@ public class DataWereHouseSaver {
 
     private State buildPackageState() {
         State state = new State();
-        state.put(DataStoreDefinition.LINES_OF_CODE.getName(), linesOfCode());
-        state.put(DataStoreDefinition.CLASSES.getName(), 5.);
-        state.put(DataStoreDefinition.EFFERENT_COUPLING_INTERNAL.getName(), 4.);
-        state.put(DataStoreDefinition.EFFERENT_COUPLING_LIBRARY.getName(), 2.);
-        state.put(DataStoreDefinition.AFFERENT_COUPLING.getName(), 4.);
+        state.put(DataStoreDefinition.CLASSES.getName(), totalOfClasses());   
+        state.put(DataStoreDefinition.LINES_OF_CODE.getName(), totalLinesOfCode());   
+        state.put(DataStoreDefinition.EMPTY_LINES_OF_CODE.getName(),emptyLinesOfCode()); 
+        state.put(DataStoreDefinition.EFFECTIVE_LINES_OF_CODE.getName(), effectiveLinesOfCode());
+        state.put(DataStoreDefinition.COMMENT_LINES_OF_CODE.getName(), commentLinesOfCode());
         return state;
     }
+    
+   
 
     private Dimension getDimension(DimensionDefinition definition) {
         return getDataStore().getDimension(definition.getName());
@@ -108,10 +113,27 @@ public class DataWereHouseSaver {
         getDataStore().insert(tuple);
     }
     
-    private double linesOfCode(){
+    private double totalLinesOfCode(){
         PackageMetricParser packageParser = new PackageMetricParser();
         return packageParser.getCodeLines(this.sourceFolder.getAbsolutePath()).getTotalCodeLines(); 
     }
-    
+     private double emptyLinesOfCode(){
+        PackageMetricParser packageParser = new PackageMetricParser();
+        return packageParser.getCodeLines(this.sourceFolder.getAbsolutePath()).getEmptyLines(); 
+    }
    
+      private double effectiveLinesOfCode(){
+        PackageMetricParser packageParser = new PackageMetricParser();
+        return packageParser.getCodeLines(this.sourceFolder.getAbsolutePath()).getEffectiveLines(); 
+    }
+        private double commentLinesOfCode(){
+        PackageMetricParser packageParser = new PackageMetricParser();
+        return packageParser.getCodeLines(this.sourceFolder.getAbsolutePath()).getCommentLines(); 
+    }
+   
+           private double totalOfClasses(){
+        PackageMetricParser packageParser = new PackageMetricParser();
+        return packageParser.getNumberOfClasses(this.sourceFolder.getAbsolutePath()); 
+    }
+    
 }
